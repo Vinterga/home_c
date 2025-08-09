b6750b43e38e926b7f40660cbb315cd8c0c36467
Tatyana Lobanova <tatyanalobanova1997@gmail.com>
2025-08-09 14:10:50 +0300

    Homework №4

---
+++ HW4/HW4_A15.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   float X1, Y1, X2, Y2, k, b;
+   scanf("%f %f %f %f", &X1, &Y1, &X2, &Y2);
+
+   k = (Y2 - Y1) / (X2 - X1); // Вычисляем угловой коэффициент k 
+   b = Y1 - k*X1; // вывели b из уравнения прямой Y = k*X + b, взять можно любую из двух точек, я взяла первую
+
+
+    printf("%.2f %.2f", k, b);
+
+
+return 0;
+
+}
